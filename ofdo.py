9c7fb7deb2b449788f6298f4347a631e9837085c
MrFlamel <jakob.luhamets@gmail.com>
2026-03-21 09:55:01 +0200

    Version bump

--- ofdo.py
+++ ofdo.py
@@ -9,7 +9,7 @@ import slaidigeneraatorid as slaidilooja
 import pyhad
 import uuendaja
 
-VERSIOON = "v106"
+VERSIOON = "v107"
 PYTHON = False # False - selleks, et python compilida; True - selleks, et testida pythonit
 
 
