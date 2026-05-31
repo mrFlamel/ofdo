5cf769844e84d0f72cd39e646116f225decca294
Jakob Luhamets <jakob.luhamets@gmail.com>
2026-05-31 18:04:16 +0300

    Windowsi toe lõpetamine praeguseks

--- build.sh
+++ build.sh
@@ -2,6 +2,3 @@ source venv/bin/activate
 
 printf '\n\n\nLINUX:\n'
 pyinstaller ofdo.spec
-
-printf '\n\n\nWINDOWS:\n'
-wine ~/.wine/drive_c/users/jakob-luhamets/AppData/Local/Programs/Python/Python312/Scripts/pyinstaller.exe ofdo.spec
